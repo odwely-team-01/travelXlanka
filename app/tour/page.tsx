e70ece03a62defcd8b1c581e9071dd8b1fd4571f
IndunilRanganath <indunilranganath071@gmail.com>
2024-10-29 07:24:33 +0530

    upadete

--- app/tour/page.tsx
+++ app/tour/page.tsx
@@ -82,7 +82,7 @@ function Tour() {
           </div>
 
           {/* Right Section */}
-          <div className="lg:col-span-3 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="lg:col-span-3 w-full grid xsm:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {packagesData?.map((tour, index) => (
               <TourCard
                 key={index}
